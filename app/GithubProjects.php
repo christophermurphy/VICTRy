4a7368fca1f5618f3665b18a4c6271bf83548465
christophermurphy <chris.murphy@tcg.com>
2020-07-12 17:04:15 -0400

    Add fillable to model

--- app/GithubProjects.php
+++ app/GithubProjects.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class GithubProjects extends Model
 {
-    //
+    protected $table = 'github_projects';
+
+    protected $fillable = ['repo_id', 'name', 'url', 'created_date', 'last_push_date', 'description', 'stargazers_count'];
 }
