e74fa7c353c6c52de7976852180e4ebc3051ea93
christophermurphy <chris.murphy@tcg.com>
2020-07-12 17:02:04 -0400

    Add GithubProject factory

--- database/factories/GithubProjectsFactory.php
+++ database/factories/GithubProjectsFactory.php
@@ -7,6 +7,12 @@ use Faker\Generator as Faker;
 
 $factory->define(GithubProjects::class, function (Faker $faker) {
     return [
-        //
+    'repo_id' => $faker->randomNumber(5),
+    'name' => $faker->name,
+    'url' => $faker->url,
+    'created_date' => $faker->dateTime,
+    'last_push_date' => $faker->dateTime,
+    'description' => $faker->text(),
+    'stargazers_count' => $faker->randomNumber(),
     ];
 });
