554f3797c38b67a62086a9e05a974bae009d6835
christophermurphy <chris.murphy@tcg.com>
2020-07-12 21:44:42 -0400

    Deal with date format for mysql

--- app/GithubProjects.php
+++ app/GithubProjects.php
@@ -9,4 +9,9 @@ class GithubProjects extends Model
     protected $table = 'github_projects';
 
     protected $fillable = ['repo_id', 'name', 'url', 'created_date', 'last_push_date', 'description', 'stargazers_count'];
+
+    public function getDateFormat()
+    {
+        return 'Y-m-d H:i:s';
+    }
 }
